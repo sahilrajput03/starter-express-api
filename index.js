8569496c3aab5c4cd940c8cefa8fcea069243185
Sahil Rajput <sahilrajput03@gmail.com>
2022-01-16 21:59:35 +0530

    Use grep path.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 // });
 
 // get route
-app.get("/:abc", (req, res) => {
+app.get("/*", (req, res) => {
   console.log("Just got a GET request!");
-  res.send(req.path + new Date());
+  res.send(req.path + " Date: " + new Date());
 });
 
 app.listen(process.env.PORT || 3001);
