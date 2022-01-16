2f91357ca6e00729d39dea25fb1a740f9fc167c6
Sahil Rajput <sahilrajput03@gmail.com>
2022-01-16 21:56:53 +0530

    Update.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // get route
 app.get("/:abc", (req, res) => {
   console.log("Just got a GET request!");
-  res.send(req.path);
+  res.send(req.path + new Date());
 });
 
 app.listen(process.env.PORT || 3001);
